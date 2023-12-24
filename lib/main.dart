8042a551927f45d7c5e940749b84bc29871d8e84
Faheem <fisforfaheem@gmail.com>
2023-12-24 19:07:15 +0500

    feat: connect Firebase DB and add ontap vote increasing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_voting_app_example/model.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -13,30 +22,41 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text('Simple voting App')),
-        body: ListView.builder(
-          itemCount: MockBandInfo.bandList.length,
-          itemExtent: 50,
-          itemBuilder: (context, index) => BandListInterface(index: index),
+        appBar: AppBar(title: const Text('Simple Voting App Using Firebase!')),
+        body: StreamBuilder<QuerySnapshot>(
+          stream:
+              FirebaseFirestore.instance.collection('candidates').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator(); // Show a loading spinner when data is loading
+            } else if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data?.docs.length,
+                itemExtent: 50,
+                itemBuilder: (context, index) {
+                  final doc = snapshot.data?.docs[index];
+                  final data =
+                      MockBandInfo.fromMap(doc?.data() as Map<String, dynamic>);
+
+                  return ListTile(
+                    title: Text(data.name),
+                    trailing: Text(data.votes.toString()),
+                    onTap: () {
+                      // Increment votes
+                      FirebaseFirestore.instance
+                          .collection('candidates')
+                          .doc(doc?.id)
+                          .update({'votes': FieldValue.increment(1)});
+                    },
+                  );
+                },
+              );
+            } else {
+              return const Text('No data');
+            }
+          },
         ),
       ),
-    );
-  }
-}
-
-class BandListInterface extends StatelessWidget {
-  final int index;
-  const BandListInterface({
-    super.key,
-    required this.index,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    // return const Text('dasdad');
-    return ListTile(
-      leading: Text(MockBandInfo.bandList[index].name),
-      trailing: Text(MockBandInfo.bandList[index].votes.toString()),
     );
   }
 }
--- lib/model.dart
+++ lib/model.dart
@@ -4,10 +4,17 @@ class MockBandInfo {
 
   MockBandInfo({required this.name, required this.votes});
 
-  static List<MockBandInfo> bandList = [
-    MockBandInfo(name: 'Metallica', votes: 5),
-    MockBandInfo(name: 'Queen', votes: 3),
-    MockBandInfo(name: 'AC/DC', votes: 2),
-    MockBandInfo(name: 'Bon Jovi', votes: 1),
-  ];
+  static MockBandInfo fromMap(Map<String, dynamic> map) {
+    return MockBandInfo(
+      name: map['name'],
+      votes: map['votes'],
+    );
+  }
+
+  // static List<MockBandInfo> bandList = [
+  //   MockBandInfo(name: 'Metallica', votes: 5),
+  //   MockBandInfo(name: 'Queen', votes: 3),
+  //   MockBandInfo(name: 'AC/DC', votes: 2),
+  //   MockBandInfo(name: 'Bon Jovi', votes: 1),
+  // ];
 }
